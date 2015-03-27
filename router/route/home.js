fb9b1d8bc11f5dc8f18eff715f79c25f36b4a583
JacobKangJie <jiekang@thoughtworks.com>
2015-03-27 11:15:31 +0800

    change homejs

--- router/route/home.js
+++ router/route/home.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var connect = require('../../model/goodsSchema.js');
+var
 
 router.get('/', function (req, res) {
   res.render('index');
+
+
+
 });
+
 
 module.exports = router;
